20256ec25fc152f6427b76b7598e54e2ccaf7092
jackduryea <jd50@rice.edu>
2019-06-24 12:47:31 -0600

    docs

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(name="prelim",
+      version="0.1.12",
+      description="Package to impute missing CpG methylation data", 
+      author="Jack Duryea",
+      author_email="jack.duryea@bcm.edu",
+      license='MIT',
+      packages=['src'],
+
+      install_requires=[
+          'pysam', 
+          'numpy', 
+          'matplotlib>3,<3.1', 
+          'scikit-learn==0.21.2',
+          'joblib',
+          'seaborn', 
+          'scipy', 
+          'pandas', 
+          'fastcluster', 
+          'pebble', 
+          'tqdm'],
+
+     
+
+)
